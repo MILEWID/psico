eeed4fc4dda33b755f80ad3865059cdf831262f4
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2022-06-15 21:49:10 -0500

    nuevo commit este si no borrar

--- src/main/java/com/example/demo/PsicoApplication.java
+++ src/main/java/com/example/demo/PsicoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PsicoApplication {
 
-	//comentario
 	public static void main(String[] args) {
+		
+		//comentarios 
 		SpringApplication.run(PsicoApplication.class, args);
 	}
 

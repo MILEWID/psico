c0c0170564005a20bdb4ab45b78449b251504eea
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2022-06-15 23:06:00 -0500

    citaMedicaController

--- src/main/java/com/example/demo/controller/citaMedicaController.java
+++ src/main/java/com/example/demo/controller/citaMedicaController.java
@@ -1,5 +1,31 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.CitaMedicaEntity;
+import com.example.demo.service.citaMedicaService;
+
+@RestController
+@RequestMapping("/citamedica")
 public class citaMedicaController {
 
+	@Autowired
+	citaMedicaService citaMedicaService;
+	
+	@GetMapping()
+	public ArrayList<CitaMedicaEntity> obtenerCitaMedica(){
+		return citaMedicaService.obtenerCitaMedica();
+	}
+	
+	@PostMapping()
+	public CitaMedicaEntity guardarCitaMedica(@RequestBody CitaMedicaEntity citaMedica) {
+		return this.citaMedicaService.guardarCitaMedica(citaMedica);
+	}
 }

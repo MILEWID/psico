470a56afe2686eac75e835d1445b2b354459796f
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2022-06-16 10:24:39 -0500

    horarioEntity

---
+++ src/main/java/com/example/demo/models/horarioEntity.java
@@ -0,0 +1,65 @@
+package com.example.demo.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="horario")
+
+public class horarioEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+	
+	
+	private Long id;
+	private String fecha;
+	private String hora;
+	private Long idCita;
+	private Long idMedico;
+	private Long idEstado;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	public Long getIdCita() {
+		return idCita;
+	}
+	public void setIdCita(Long idCita) {
+		this.idCita = idCita;
+	}
+	public Long getIdMedico() {
+		return idMedico;
+	}
+	public void setIdMedico(Long idMedico) {
+		this.idMedico = idMedico;
+	}
+	public Long getIdEstado() {
+		return idEstado;
+	}
+	public void setIdEstado(Long idEstado) {
+		this.idEstado = idEstado;
+	}
+	
+	
+
+}

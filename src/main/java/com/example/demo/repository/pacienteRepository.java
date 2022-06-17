bca4d950da625bd40e35bbc311b7096232fd236e
sebas <78992252+SebasMena28@users.noreply.github.com>
2022-06-16 20:03:13 -0500

    casi la cago
    jeje

--- src/main/java/com/example/demo/repository/pacienteRepository.java
+++ src/main/java/com/example/demo/repository/pacienteRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
  *
  * @author Usuario
  */
+@Repository
 public interface pacienteRepository extends CrudRepository<pacienteEntity , Long> {
     
 }

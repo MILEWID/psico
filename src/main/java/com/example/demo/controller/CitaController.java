28476bce66b4ad282a089ed91cd619436ba8ad8a
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2022-06-21 19:54:35 -0500

    Tabla cita

---
+++ src/main/java/com/example/demo/controller/CitaController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.models.CitaEntity;
+import com.example.demo.service.CitaService;
+
+@RestController
+
+@RequestMapping("/cita")
+
+public class CitaController {
+	
+	@Autowired
+	
+	CitaService cita;
+	
+	@GetMapping()
+	public ArrayList<CitaEntity> obtenerCita(){
+		return cita.obtenerCita();
+	}
+	
+	@PostMapping()
+	public CitaEntity guardarCita(@RequestBody CitaEntity cita){
+		return this.cita.guardarCita(cita);
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public String eliminarCita(@PathVariable("id")Long id){
+		boolean ok=this.cita.eliminarCita(id);
+		if(ok) {
+			return "La cita se eliminó correctamente";
+		}else {
+			return "La cita no se eliminó";
+		}
+	}
+
+}
---
+++ src/main/java/com/example/demo/service/CitaService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.CitaEntity;
+import com.example.demo.repository.CitaRepository;
+
+@Service
+
+public class CitaService {
+	
+	@Autowired
+	CitaRepository cita;
+	
+	public ArrayList<CitaEntity> obtenerCita(){
+		return (ArrayList<CitaEntity>)cita.findAll();
+	}
+	
+	public CitaEntity guardarCita(CitaEntity citas) {
+		return cita.save(citas);
+	}
+	
+	public Optional<CitaEntity> obtenerPorId(Long id){
+		return cita.findById(id);
+	}
+	
+	public boolean eliminarCita(Long id) {
+		try {
+			cita.deleteById(id);
+			return true;
+		}catch(Exception err) {
+			return false;
+		}
+	}
+	
+
+}
